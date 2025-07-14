0dc84682c36807c5d9a60cc87eaafbad8af747fa
angovnable <brywanyoike@gmail.com>
2025-07-15 02:28:24 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
-
-const CACHE='roe-v1';
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','index.html','icon-512.png','manifest.json'])));
+self.addEventListener("install", e => {
+  console.log("Service Worker installed");
 });
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request));
 });
